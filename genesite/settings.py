f5b25bfe6aecb70f074e5f1171870ddab8f4c139
Wesley Persad <vasudeo.persad@gmail.com>
2024-07-04 12:18:16 +0100

    Gunicorn added

--- genesite/settings.py
+++ genesite/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = env('SECRET_KEY')
 HOSTNAME = env('HOSTNAME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 ALLOWED_HOSTS = [HOSTNAME]
